08070def0e6b2cc38d5d4189846fa829f688fe61
shankuljain <shankuljain@Shankuls-MacBook-Air.local>
2015-08-07 05:50:58 +0530

    Initial Commit

---
+++ Six Degrees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Six Degrees
+//
+//  Created by Shankul Jain on 07/08/15.
+//  Copyright (c) 2015 Shankul Jain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
